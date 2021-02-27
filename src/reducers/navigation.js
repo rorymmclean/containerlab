e7d4d0d58f7bf555e6ceda24907171703642b8c0
rorymmclean <rory.mclean@verizon.net>
2021-02-27 10:52:24 -0500

    adding template

---
+++ src/reducers/navigation.js
@@ -0,0 +1,592 @@
+const urls = [
+  {
+    title: 'Navigation',
+    items: [
+      {
+        url: '',
+        icon: 'dashboard',
+        title: 'Dashboards',
+        badge: {
+          className: 'badge badge-danger badge-sm badge-outline',
+          title: 'New'
+        },
+        items: [
+          {
+            url: '/dashboards/analytics',
+            icon: '',
+            title: 'Analytics',
+            items: []
+          },
+          {
+            url: '/dashboards/geographic',
+            icon: '',
+            title: 'Geographic',
+            items: []
+          },
+          {
+            url: '/dashboards/e-commerce',
+            icon: '',
+            title: 'E-commerce',
+            items: []
+          }
+        ]
+      },
+      {
+        url: '',
+        icon: 'bookmark_border',
+        title: 'Demos',
+        items: [
+          {
+            url: '/demos/demo-1',
+            icon: '',
+            title: 'Demo 1',
+            items: []
+          },
+          {
+            url: '/demos/demo-2',
+            icon: '',
+            title: 'Demo 2',
+            items: []
+          },
+          {
+            url: '/demos/demo-3',
+            icon: '',
+            title: 'Demo 3',
+            items: []
+          },
+          {
+            url: '/demos/demo-4',
+            icon: '',
+            title: 'Demo 4',
+            items: []
+          },
+          {
+            url: '/demos/demo-5',
+            icon: '',
+            title: 'Demo 5',
+            items: []
+          },
+          {
+            url: '/demos/demo-6',
+            icon: '',
+            title: 'Demo 6',
+            items: []
+          }
+        ]
+      },
+      {
+        url: '',
+        icon: 'code',
+        title: 'Layouts',
+        badge: {
+          className: 'badge badge-info badge-sm badge-rounded badge-outline',
+          title: '3'
+        },
+        items: [
+          {
+            url: '/layouts/default-sidebar-1',
+            icon: '',
+            title: 'Default sidebar 1',
+            items: []
+          },
+          {
+            url: '/layouts/collapsed-sidebar-1',
+            icon: '',
+            title: 'Collapsed sidebar 1',
+            items: []
+          },
+          {
+            url: '/layouts/top-navigation-1',
+            icon: '',
+            title: 'Top navigation 1',
+            items: []
+          }
+        ]
+      },
+      {
+        url: '',
+        icon: 'extension',
+        title: 'Widgets',
+        items: [
+          {
+            url: '/widgets/activity-widgets',
+            icon: '',
+            title: 'Activity widgets',
+            items: []
+          },
+          {
+            url: '/widgets/area-chart-widgets',
+            icon: '',
+            title: 'Area chart widgets',
+            items: []
+          },
+          {
+            url: '/widgets/bar-chart-widgets',
+            icon: '',
+            title: 'Bar chart widgets',
+            items: []
+          },
+          {
+            url: '/widgets/donut-chart-widgets',
+            icon: '',
+            title: 'Donut chart widgets',
+            items: []
+          },
+          {
+            url: '/widgets/icon-widgets',
+            icon: '',
+            title: 'Icon widgets',
+            items: []
+          },
+          {
+            url: '/widgets/line-chart-widgets',
+            icon: '',
+            title: 'Line chart widgets',
+            items: []
+          },
+          {
+            url: '/widgets/pie-chart-widgets',
+            icon: '',
+            title: 'Pie chart widgets',
+            items: []
+          },
+          {
+            url: '/widgets/table-widgets',
+            icon: '',
+            title: 'Table widgets',
+            items: []
+          },
+          {
+            url: '/widgets/text-widgets',
+            icon: '',
+            title: 'Text widgets',
+            items: []
+          },
+          //          {
+          //            url: '/widgets/timeline-widgets',
+          //            icon: '',
+          //            title: 'Timeline widgets',
+          //            items: []
+          //          },
+          {
+            url: '/widgets/user-widgets',
+            icon: '',
+            title: 'User widgets',
+            items: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Components',
+    items: [
+      {
+        url: '',
+        icon: 'label',
+        title: 'UI Elements',
+        items: [
+          {
+            url: '/ui-elements/badges',
+            icon: '',
+            title: 'Badges',
+            items: []
+          },
+          {
+            url: '/ui-elements/breadcrumbs',
+            icon: '',
+            title: 'Breadcrumbs',
+            items: []
+          },
+          {
+            url: '/ui-elements/buttons',
+            icon: '',
+            title: 'Buttons',
+            items: []
+          },
+          //          {
+          //            url: '/ui-elements/cards',
+          //            icon: '',
+          //            title: 'Cards',
+          //            items: []
+          //          },
+          //          {
+          //            url: '/ui-elements/dropdowns',
+          //            icon: '',
+          //            title: 'Dropdowns',
+          //            items: []
+          //          },
+          {
+            url: '/ui-elements/images',
+            icon: '',
+            title: 'Images',
+            items: []
+          },
+          {
+            url: '/ui-elements/lists',
+            icon: '',
+            title: 'Lists',
+            items: []
+          },
+          {
+            url: '/ui-elements/pagination',
+            icon: '',
+            title: 'Pagination',
+            items: []
+          },
+          {
+            url: '/ui-elements/progress-bars',
+            icon: '',
+            title: 'Progress bars',
+            items: []
+          },
+          {
+            url: '/ui-elements/social-media-buttons',
+            icon: '',
+            title: 'Social media buttons',
+            items: []
+          },
+          {
+            url: '/ui-elements/tabs',
+            icon: '',
+            title: 'Tabs',
+            items: []
+          },
+          {
+            url: '/ui-elements/typography',
+            icon: '',
+            title: 'Typography',
+            items: []
+          }
+        ]
+      },
+      {
+        url: '',
+        icon: 'assignment',
+        title: 'Forms',
+        badge: {
+          className: 'badge badge-success badge-sm badge-outline',
+          title: 'Important'
+        },
+        items: [
+          {
+            url: '/forms/default-forms',
+            icon: '',
+            title: 'Default forms',
+            items: []
+          },
+          {
+            url: '/forms/input-groups',
+            icon: '',
+            title: 'Input groups',
+            items: []
+          },
+          //          {
+          //            url: '/forms/steps',
+          //            icon: '',
+          //            title: 'Form steps',
+          //            items: []
+          //          },
+          {
+            url: '/forms/validation',
+            icon: '',
+            title: 'Form validation',
+            items: []
+          },
+          {
+            url: '/forms/sliders',
+            icon: '',
+            title: 'Sliders',
+            items: []
+          },
+          {
+            url: '/forms/react-datetime',
+            icon: '',
+            title: 'Date picker',
+            items: []
+          },
+          {
+            url: '/forms/react-select',
+            icon: '',
+            title: 'React select',
+            items: []
+          }
+        ]
+      },
+      {
+        url: '',
+        icon: 'format_list_bulleted',
+        title: 'Tables',
+        items: [
+          {
+            url: '/tables/reactable',
+            icon: '',
+            title: 'Reactable',
+            items: []
+          },
+          {
+            url: '/tables/default-tables',
+            icon: '',
+            title: 'Default tables',
+            items: []
+          }
+        ]
+      },
+      {
+        url: '',
+        icon: 'notifications',
+        title: 'Notifications',
+        badge: {
+          className: 'badge badge-warning badge-sm badge-rounded badge-outline',
+          title: '3'
+        },
+        items: [
+          {
+            url: '/ui-elements/alerts',
+            icon: '',
+            title: 'Alerts',
+            items: []
+          },
+          {
+            url: '/ui-elements/modals',
+            icon: '',
+            title: 'Modals',
+            items: []
+          },
+          {
+            url: '/notifications/reapop',
+            icon: '',
+            title: 'Reapop',
+            items: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Other',
+    items: [
+      {
+        url: '',
+        icon: 'map',
+        title: 'Maps',
+        badge: {
+          className: 'badge badge-danger badge-sm badge-rounded badge-outline',
+          title: '3'
+        },
+        items: [
+          {
+            url: '/maps/google-maps',
+            icon: '',
+            title: 'Google maps',
+            items: []
+          },
+          {
+            url: '/maps/vector-maps',
+            icon: '',
+            title: 'Vector maps',
+            items: []
+          }
+        ]
+      },
+      {
+        url: '',
+        icon: 'explore',
+        title: 'Icons',
+        badge: {
+          className: 'badge badge-info badge-sm badge-outline',
+          title: 'New'
+        },
+        items: [
+          {
+            url: '/icons/flags',
+            icon: '',
+            title: 'Flags',
+            items: []
+          },
+          {
+            url: '/icons/font-awesome',
+            icon: '',
+            title: 'Font awesome',
+            items: []
+          },
+          {
+            url: '/icons/ionicons',
+            icon: '',
+            title: 'Ionicons',
+            items: []
+          },
+          {
+            url: '/icons/material-design-icons',
+            icon: '',
+            title: 'Material design icons',
+            items: []
+          },
+          {
+            url: '/icons/simple-line-icons',
+            icon: '',
+            title: 'Simple line icons',
+            items: []
+          },
+          {
+            url: '/icons/weather-icons',
+            icon: '',
+            title: 'Weather icons',
+            items: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Extras',
+    items: [
+      {
+        url: '',
+        icon: 'insert_drive_file',
+        title: 'Pages',
+        items: [
+          {
+            url: '/pages/contact-us',
+            icon: '',
+            title: 'Contact us',
+            items: []
+          },
+          {
+            url: '/pages/create-account',
+            icon: '',
+            title: 'Create account',
+            items: []
+          },
+          {
+            url: '/pages/empty-page',
+            icon: '',
+            title: 'Empty page',
+            items: []
+          },
+          {
+            url: '/pages/error-page',
+            icon: '',
+            title: 'Error page',
+            items: []
+          },
+          {
+            url: '/pages/login',
+            icon: '',
+            title: 'Login',
+            items: []
+          },
+          {
+            url: '/pages/reset-password',
+            icon: '',
+            title: 'Reset password',
+            items: []
+          },
+          {
+            url: '/pages/subscribe',
+            icon: '',
+            title: 'Subscribe',
+            items: []
+          },
+          {
+            url: '/pages/under-maintenance',
+            icon: '',
+            title: 'Under maintenance',
+            items: []
+          },
+          {
+            url: '/pages/unlock-account',
+            icon: '',
+            title: 'Unlock account',
+            items: []
+          }
+          //          {
+          //            url: '/pages/user-profile',
+          //            icon: '',
+          //            title: 'User profile',
+          //            items: []
+          //          }
+        ]
+      },
+      {
+        url: '',
+        icon: 'pie_chart',
+        title: 'Charts',
+        badge: {
+          className: 'badge badge-primary badge-sm badge-rounded badge-outline',
+          title: '2'
+        },
+        items: [
+          {
+            url: '/charts/chartist',
+            icon: '',
+            title: 'Chartist',
+            items: []
+          },
+          {
+            url: '/charts/recharts',
+            icon: '',
+            title: 'Recharts',
+            items: []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Docs',
+    items: [
+      {
+        url: '',
+        icon: 'info',
+        title: 'Documentation',
+        badge: {
+          className: 'badge badge-danger badge-sm badge-outline',
+          title: 'New'
+        },
+        items: [
+          {
+            url: '/documentation/installation',
+            icon: '',
+            title: 'Installation',
+            items: []
+          },
+          {
+            url: '/documentation/code-structure',
+            icon: '',
+            title: 'Code structure',
+            items: []
+          },
+          {
+            url: '/documentation/faq',
+            icon: '',
+            title: 'FAQ',
+            items: []
+          },
+          {
+            url: '/documentation/change-log',
+            icon: '',
+            title: 'Change log',
+            items: []
+          },
+          {
+            url: '/documentation/credits',
+            icon: '',
+            title: 'Credits',
+            items: []
+          }
+        ]
+      }
+    ]
+  }
+]
+export function navigation(state = Array.from(urls), action) {
+  switch (action.type) {
+    case 'SET_NAVIGATION':
+      return Object.assign({}, state, {
+        ...action.navigation
+      })
+    default:
+      return state
+  }
+}
