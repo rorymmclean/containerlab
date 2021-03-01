4c66d43a210a06f82ddcc3cd922c881d2d9b55b0
rorymmclean <rory.mclean@verizon.net>
2021-03-01 09:39:25 -0500

    web tweeks

---
+++ test.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#The script demonstrates the use of variables entered at the command line.
+echo "My name is $1"
+echo "I work for $2"
--- web.py
+++ web.py
@@ -2,11 +2,12 @@ import os, os.path
 import random
 import string
 import cherrypy
+import psutil
+import shutil
 import docker
 import sqlite3
 client = docker.from_env()
 DB_STRING = "dockermgr.db"
-#conn = sqlite3.connect('dockermgr.db')
 
 html_top = """<html>
           <head>
@@ -23,6 +24,8 @@ html_top = """<html>
                  .footer {
                  	position: fixed;
 					bottom: -10px;
+					height: 100px;
+					min-height: 100px;
 					width: 100%;
                  }
                  
@@ -52,16 +55,29 @@ html_top = """<html>
                      <li class="list-item"><a class="list-link" href="/subscriptions">Subscriptions</a></li>
                   </ul>
                   <ul class="list-unstyled">
-                     <li class="list-item"><a class="list-link" href="/other">Other</a></li>
+                     <li class="list-item"><a class="list-link" href="/stats">Stats</a></li>
                   </ul>
                </div>
              </div>"""
 
 html_footer = """ 
-             <div data-background="light" data-navbar="primary" 
-               data-top-navigation="primary" data-logo="light" data-left-sidebar="light" 
-               data-collapsed="false">
-             <div class="navbar navbar-2 justify-content-around align-items-center h6 footer" >footer</div>
+             <div data-navbar="primary">
+             <div class="navbar navbar-2 justify-content-around align-items-center h6 footer" style="min-height:90px;">
+                 <div style="width: 33%; height: 90%; float:left;">
+                    <h5 style="margin-bottom: 0;"><a href="http://www.poc2ops.com" target="_blank">Poc2Ops.com</a></h5>
+                    Street<br/>
+                    City, State 12345
+                 </div>
+                 <div style="width: 33%; height: 90%; float:center;">
+                    Email: <a href="mailto:info@poc2ops.com">info@Poc2Ops.com</a><br/>
+                    Phone: TBD<br/>
+                    Fax: 703-935-4603
+                 </div>
+                 <div style="width: 33%; height: 90%; float:right;">
+                    Poc2Ops can help you implement a <I>Open Source First</I> strategy.<br/>
+
+                 </div>
+             </div>
              </div>  
           </div>  
           </body>
@@ -83,11 +99,16 @@ class StringGenerator(object):
         
     @cherrypy.expose
     def labs(self):
+        loadrate = float(list(psutil.getloadavg())[1])/float(psutil.cpu_count())
+        if loadrate > 1:
+            button_flag = 'disabled'
+        else:
+            button_flag = ''
         html_body = """<div class="col-6 corebody"><h2 class="text-center">Available Labs</h2><table class="table table-unbordered table-striped thead-inverse"><thead><tr><th>Lab Name</th><th style="text-align:center">Duration</th><th style="text-align:center">Launch</th></tr></thead>"""
         with sqlite3.connect(DB_STRING) as c:
             r_cursor = c.execute('SELECT id, appname, default_dur from APPS;') 
         for row in r_cursor:
-            html_body = html_body + '<tr><td>'+str(row[1])+'</td><td style="text-align:center">'+str(row[2])+'</td><td style="text-align:center"><button class="btn btn-primary btn-rounded">Launch</button>'+str(row[0])+'</td></tr>'                          
+            html_body = html_body + '<tr><td>'+str(row[1])+'</td><td style="text-align:center">'+str(row[2])+'</td><td style="text-align:center"><button class="btn btn-primary btn-rounded" '+button_flag+' app="'+str(row[0])+'">Launch</button></td></tr>'                          
         html_body = html_body + """</table></div>"""            
         html_return = html_top + html_body + html_footer
         return html_return
@@ -106,14 +127,23 @@ class StringGenerator(object):
         return html_return
 
     @cherrypy.expose
-    def other(self):
-        html_body = """<div class="col main corebody">
-                        Enter Body Text Here
-                        </div>"""
-             
+    def stats(self):
+        loadrate = round(float(list(psutil.getloadavg())[1])/float(psutil.cpu_count())*100,2)
+        freespace = str(round(list(shutil.disk_usage("/"))[2]/(1024*1024*1024),1))+' gb'
+        curr_cont = str(len(client.containers.list()))
+        curr_images = str(len(client.images.list()))
+        
+        html_body = """<div class="col-4 corebody">
+            <table class="table table-unbordered table-striped thead-inverse">
+            <thead><tr><th>Statistic</th><th>Metric</th></tr></thead>
+            <tr><td>Load Rate (%)</td><td>"""+str(loadrate)+"""</td></tr>
+            <tr><td>Free Space</td><td>"""+freespace+"""</td></tr>
+            <tr><td># Containers</td><td>"""+curr_cont+"""</td></tr>
+            <tr><td># Images</td><td>"""+curr_images+"""</td></tr>
+            </table></div>"""
         html_return = html_top + html_body + html_footer
         return html_return
-        
+
 
 if __name__ == '__main__':
     conf = {
