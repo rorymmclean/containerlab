9d95fbfcd6f61da1bd7dd9b5e9829f0e36a6b136
rorymmclean <rory.mclean@verizon.net>
2021-03-07 18:23:08 -0500

    concurrency fix

--- cleanup.py
+++ cleanup.py
@@ -1,20 +1,25 @@
 import time
 import sqlite3
 import subprocess
+from datetime import datetime
 
 DB_STRING = "dockermgr.db"
 
 while True:
+    sleep_timer = 120
     with sqlite3.connect(DB_STRING) as c:
          r_cursor = c.execute('''SELECT id, appname, starttime, default_dur from SUBSCRIPTION 
-            where DATETIME('now') > DATETIME(starttime,"+"||default_dur||" minute");''') 
+            where DATETIME('now') > DATETIME(starttime,"+"||default_dur||" minute") limit 1;''') 
     for row in r_cursor:
         print(row[0])
-        ### Kill Lab
-        lablaunch = subprocess.run(["bash", "killlab.sh", row[0]], stdout=subprocess.PIPE)
 		### Update Table
-        with sqlite3.connect(DB_STRING) as c:
+        with sqlite3.connect(DB_STRING, 10) as c:
              c.execute('DELETE FROM SUBSCRIPTION WHERE ID = :var1',[row[0]])
              c.commit()
-    time.sleep(120)
+        c.close()     
+        ### Kill Lab
+        print('Removed at: ',datetime.now())
+        lablaunch = subprocess.run(["bash", "killlab.sh", row[0]], stdout=subprocess.PIPE)
+        sleep_timer = 10
+    time.sleep(sleep_timer)
 
