1189a0b33170dcf0efe8163cf4c542fd00fb6aed
rorymmclean <rory.mclean@verizon.net>
2021-03-09 17:54:10 -0500

    runall updates

--- runall.sh
+++ runall.sh
@@ -1,4 +1,4 @@
-nohup python3 web.py >web.log & 
-nohup python3 cleanup.py >cleanup.log & 
+nohup python3 -u web.py > web.log 2>&1 & echo $! > web.pid
+nohup python3 -u cleanup.py > cleanup.log 2>&1 & echo $! > cleanup.pid
 
 
